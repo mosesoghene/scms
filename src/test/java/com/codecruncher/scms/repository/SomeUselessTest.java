af85eb32050a4f9f332bad12650cdb84c0a7bdcb
Moses Oghene <mosesogheneo@gmail.com>
2025-03-14 04:48:46 +0100

    Base project structure

---
+++ src/test/java/com/codecruncher/scms/repository/SomeUselessTest.java
@@ -0,0 +1,14 @@
+package com.codecruncher.scms.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeUselessTest {
+
+    @Test
+    public void someUselessTest(){
+        assertEquals(2 + 2, 4);
+    }
+}
